82d9248e8ce2634b6490277e6f7512d6b058a08a
Ahsan Mureed <ahsanmureed00@gmail.com>
2025-08-18 11:04:06 +0500

    okay

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import logo from '../../public/logo.png'
 
 const NavBar = () => {
   return (
-    <nav className="bg-white shadow-sm border-b border-gray-200">
+    <nav className="bg-white fixed w-full shadow-sm border-b border-gray-200 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo Section */}
